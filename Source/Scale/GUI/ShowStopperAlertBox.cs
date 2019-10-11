4942f47395b4c1bb5b40c8896a72b71aa80be22f
Lisias T <me@lisias.net>
2019-10-11 01:35:18 -0300

    "Typo maldito, typo maldito... Tralálálálálálá..."

--- Source/Scale/GUI/ShowStopperAlertBox.cs
+++ Source/Scale/GUI/ShowStopperAlertBox.cs
@@ -9,7 +9,7 @@ namespace TweakScale.GUI
     {
         private static readonly string MSG = @"Unfortunately TweakScale found {0} **FATAL** issue(s) on your KSP installment! This probably will corrupt your savagames sooner or later!
 
-The KSP.log is listing every compromised part(s) on your installment, look for lines with '[TweakScale] ERROR: **FATAL**' on the log line. Be aware that the parts being reported are not the culprits, but the Screaming Victims. There's no possiuble automated fix for these.";
+The KSP.log is listing every compromised part(s) on your installment, look for lines with '[TweakScale] ERROR: **FATAL**' on the log line. Be aware that the parts being reported are not the culprits, but the Screaming Victims. There's no possible automated fix for these.";
 
         private static readonly string AMSG = @"call for help on the TweakScale thread on the Forum (KSP will close). We will help you on diagnosing the Add'On that is troubling you. Publish your KSP.log on some file share service and mention it on the post";
 
