e99144425354f77158bb194bd88ccfbb3a02dc1c
Lisias T <me@lisias.net>
2019-10-11 01:00:13 -0300

    Slightly less naive on handling styling of the Common Dialogs

--- Source/Scale/GUI/CommonBox.cs
+++ Source/Scale/GUI/CommonBox.cs
@@ -6,39 +6,61 @@ namespace TweakScale.GUI
 {
     internal class CommonBox : TimedMessageBox
     {
+        private static GUIStyle winStyle = null;
         internal static GUIStyle createWinStyle ()
         {
-            GUIStyle style = new GUIStyle("Window")
+            if (null == winStyle)
             {
-                fontSize = 22,
-                fontStyle = FontStyle.Bold
-            };
-            style.normal.textColor = Color.yellow;
-            style.border.top = 8;
-
-            return style;
-        }
-
-        internal static GUIStyle createTextStyle ()
-        {
-            GUIStyle style = new GUIStyle("Label")
-            {
-                fontSize = 14,
-                fontStyle = FontStyle.Normal,
-                alignment = TextAnchor.MiddleLeft
-            };
-            style.padding.top = 8;
-            style.padding.bottom = style.padding.top;
-            style.padding.left = style.padding.top;
-            style.padding.right = style.padding.top;
-            {
-                Texture2D tex = new Texture2D(1,1);
-                tex.SetPixel(0,0,new Color(0f, 0f, 0f, 0.45f));
-                tex.Apply();
-                style.normal.background = tex;
+                winStyle = new GUIStyle("Window")
+                {
+                    fontSize = 22,
+                    fontStyle = FontStyle.Bold,
+                    alignment = TextAnchor.UpperCenter,
+                    wordWrap = false
+                };
+                winStyle.focused.textColor = winStyle.normal.textColor = Color.yellow;
+                winStyle.active.textColor = Color.white;
+                winStyle.border.top = 5;
+                { 
+                    Texture2D tex = new Texture2D(1,1);
+                    tex.SetPixel(0,0,new Color(0f, 0f, 0f, 0.45f));
+                    tex.Apply();
+                    winStyle.normal.background = tex;
+                }
             }
 
-            return style;
+            return winStyle;
+        }
+
+        private static GUIStyle textStyle = null;
+        internal static GUIStyle createTextStyle ()
+        {
+            if (null == textStyle)
+            {
+                textStyle = new GUIStyle("Label")
+                {
+                    fontSize = 14,
+                    fontStyle = FontStyle.Normal,
+                    alignment = TextAnchor.MiddleLeft,
+                    wordWrap = true
+                };
+                textStyle.active.textColor =
+                    textStyle.focused.textColor =
+                    textStyle.normal.textColor = Color.white;
+                textStyle.padding.top = 8;
+                textStyle.padding.bottom = textStyle.padding.top;
+                textStyle.padding.left = textStyle.padding.top;
+                textStyle.padding.right = textStyle.padding.top;
+                {
+                    Texture2D tex = new Texture2D(1,1);
+                    tex.SetPixel(0,0,new Color(0f, 0f, 0f, 0.45f));
+                    tex.Apply();
+                    textStyle.active.background =
+                        textStyle.focused.background =
+                        textStyle.normal.background = tex;
+                }
+            }
+            return textStyle;
         }
     }
 }
