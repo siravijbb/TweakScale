01cb90fee1287b2720edc1a6f8526351574ce4ce
Lisias T <me@lisias.net>
2019-10-18 04:05:30 -0300

    Moving pre-initialization code to it's own class and file. Saner.

---
+++ Source/Scale/Startup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+using TweakScale.Annotations;
+
+namespace TweakScale
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    internal class Startup : MonoBehaviour
+	{
+        [UsedImplicitly]
+        private void Start()
+        {
+            Log.init();
+            Log.force("Version {0}", Version.Text);
+
+            try
+            {
+                KSPe.Util.Compatibility.Check<Startup>(typeof(Version), typeof(Configuration));
+                KSPe.Util.Installation.Check<Startup>(typeof(Version));
+            }
+            catch (KSPe.Util.InstallmentException e)
+            {
+                Log.error(e.ToShortMessage());
+                KSPe.Common.Dialogs.ShowStopperAlertBox.Show(e);
+            }
+        }
+	}
+}
