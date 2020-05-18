067a05c23b654b7a6939536cedd02526c20d6fb9
Lisias <me@lisias.net>
2020-05-18 18:28:20 -0300

    Tyops!

--- Source/Scale_Redist/Updater.cs
+++ Source/Scale_Redist/Updater.cs
@@ -4,14 +4,14 @@
 		© 2015-2018 pellinor
 		© 2014 Gaius Godspeed and Biotronic
 
-		THIE FILE is licensed to you under:
+		THIS FILE is licensed to you under:
 
 		* WTFPL - http://www.wtfpl.net
 			* Everyone is permitted to copy and distribute verbatim or modified
  			    copies of this license document, and changing it is allowed as long
 				as the name is changed.
 
-		THIE FILE is distributed in the hope that it will be useful,
+		THIS FILE is distributed in the hope that it will be useful,
 		but WITHOUT ANY WARRANTY; without even the implied warranty of
 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 */
