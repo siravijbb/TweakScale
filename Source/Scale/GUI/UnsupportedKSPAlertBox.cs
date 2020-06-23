aa27d8d7cd76b806cb05c7b9252d5db06badbaf4
Lisias <me@lisias.net>
2020-06-23 10:05:28 -0300

    Redirecting user to Github issue #115 when KSP 1.10 is launched.
    https://github.com/net-lisias-ksp/TweakScale/issues/115

--- Source/Scale/GUI/UnsupportedKSPAlertBox.cs
+++ Source/Scale/GUI/UnsupportedKSPAlertBox.cs
@@ -10,14 +10,14 @@ namespace TweakScale.GUI
 
 It's not certain that it will not work fine, it's **NOT KNOWN** and if anything goes wrong, KSP will inject bad information on your savegames corrupting parts with TwekScale.";
 
-        private static readonly string AMSG = @"check the TweakScale thread on the Forum (KSP will close) to see any news about KSP 1.10.";
+        private static readonly string AMSG = @"check the GitHub issue #115 (KSP will close) to be updated on any news about KSP 1.10";
 
         internal static void Show()
         {
             KSPe.Common.Dialogs.ShowStopperAlertBox.Show(
                 MSG,
                 AMSG,
-                () => { Application.OpenURL("https://forum.kerbalspaceprogram.com/index.php?/topic/179030-*"); Application.Quit(); }
+                () => { Application.OpenURL("https://github.com/net-lisias-ksp/TweakScale/issues/115"); Application.Quit(); }
             );
             Log.force("\"Houston, we have a Problem!\" about KSP 1.10 was displayed");
         }
